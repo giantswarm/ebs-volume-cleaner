1aa6948d180a6d4f0d862b6d7cd444460f074bf2
Pau <paurosello@gmail.com>
2022-07-11 15:28:44 +0200

    Add master filter

--- clean_ebs.sh
+++ clean_ebs.sh
@@ -9,16 +9,19 @@ volumesInCluster_amount=$(echo $volumesInCluster | tr ' ' '\n' | wc -l)
 volumesInCluster2_amount=$(echo $volumesInCluster2 | tr ' ' '\n' | wc -l)
 total_incluster=$((volumesInCluster_amount + volumesInCluster2_amount))
 
+etcdVolumesInAws=$(aws ec2 describe-volumes --no-paginate --filters=Name=tag:kubernetes.io/cluster/$CLUSTER,Values=owned --filters=Name=tag:aws:cloudformation:stack-name,Values=cluster-$CLUSTER-tccpn --query "Volumes[*].{ID:VolumeId}" | jq .[].ID -r | sort | uniq -u)
+
 volumesInAws=$(aws ec2 describe-volumes --no-paginate --filters=Name=tag:kubernetes.io/cluster/$CLUSTER,Values=owned --query "Volumes[*].{ID:VolumeId}" | jq .[].ID -r | sort | uniq -u)
 volumesInAws_amount=$(echo $volumesInAws | tr ' ' '\n' | wc -l | sed 's/ //g' )
 
 echo "There are $total_incluster in the cluster $CLUSTER"
 echo "There are $volumesInAws_amount in the AWS account of cluster $CLUSTER"
 
-missing=(`echo ${volumesInCluster[@]} ${volumesInCluster2[@]} ${volumesInAws[@]} | tr ' ' '\n' | sort | uniq -u`)
+missing=(`echo ${volumesInCluster[@]} ${volumesInCluster2[@]} ${etcdVolumesInAws[@]} ${volumesInAws[@]} | tr ' ' '\n' | sort | uniq -u`)
 echo "The are ${#missing[@]} volumes not present in the kubernetes cluster:"
 for id in ${missing[@]}; do
-  echo $id
+  volume_status=$(aws ec2 describe-volumes --volume-ids $id --query "Volumes[*].{State:State}" | jq .[].State -r)
+  echo "Volume $id is in status: $volume_status"
 done
 
 echo "If you want to delete the volumes execute the following commands:"
